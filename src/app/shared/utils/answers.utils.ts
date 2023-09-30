fd3381982f725e01a532c8964964e701bc911be1
Maciej Hajduk <maciej.hajduk@deviniti.com>
2023-10-01 01:03:50 +0200

    build: label fixes

--- src/app/shared/utils/answers.utils.ts
+++ src/app/shared/utils/answers.utils.ts
@@ -5,12 +5,12 @@ export const mockedAnswerBySurvey = (
 ): string => {
   switch (satisfaction) {
     case 'Kontakt z innymi ludźmi: długie rozmowy, wymiana doświadczeń i opinii':
-      return 'Widzę, ze mamy do czynienia z przyszłym managerem. Dobrze, powiedz mi jeszcze jakie są Twoje mocne strony?';
+      return 'Zdaje się, ze mamy do czynienia z przyszłym managerem. Dobrze, powiedz mi jeszcze jakie są Twoje mocne strony?';
     case 'Zajęcia kreatywne: rysowanie, malowanie, komponowanie':
-      return 'Widzę, ze mamy do czynienia z humanistą! Dobrze, powiedz mi jeszcze jakie są Twoje mocne strony?';
+      return 'Zdaje się, ze mamy do czynienia z humanistą! Dobrze, powiedz mi jeszcze jakie są Twoje mocne strony?';
     case 'Analizowanie danych, wyciąganie wniosków i tworzenie ciekawych rozwiązań':
       return 'Wygląda na to, że rozmawiam ze ścisłym umysłem! Dobrze, powiedz mi jeszcze jakie są Twoje mocne strony?';
     default:
-      return 'W porządku! Świetnie się składa! Powiedz w takim razie jaka ścieżka kariery cię interesuje!';
+      return 'W porządku, świetnie się składa! Powiedz w takim razie jaka ścieżka kariery cię interesuje?';
   }
 };
