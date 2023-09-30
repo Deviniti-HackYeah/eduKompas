090f1d36b6552f0b2ba65a97eaec4d5325be9743
Maciej Hajduk <maciej.hajduk@deviniti.com>
2023-09-30 16:11:42 +0200

    build: chat bubbles

--- src/app/core/components/message/message.component.ts
+++ src/app/core/components/message/message.component.ts
@@ -11,6 +11,22 @@ export class MessageComponent {
   @Input() public direction?: 'left' | 'right' = 'right';
   @Input() public type?: Bot | 'USER' = 'USER';
 
+  public get bubbleBgColor(): string {
+    return this.type === 'USER'
+      ? 'bg-yellow-100 border-yellow-100'
+      : this.type === 'KARA'
+      ? 'bg-red-100 border-red-100'
+      : 'bg-blue-100 border-blue-100';
+  }
+
+  public get bubbleAvatar(): string {
+    return this.type === 'USER'
+      ? 'assets/images/user.png'
+      : this.type === 'KARA'
+      ? 'assets/images/kara.png'
+      : 'assets/images/kajtek.png';
+  }
+
   @HostBinding('class') public get classes(): string {
     const align = this.direction === 'left' ? 'justify-start' : 'justify-end';
     return ['flex', 'mb-2', align].join(' ');
